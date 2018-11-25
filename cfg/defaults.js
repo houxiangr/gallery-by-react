33609fba6ff1f430f206f140f3dde68a25f61f61
houxiangrui <houxiangrui@baidu.com>
2018-11-25 12:18:57 +0800

    make the stage

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -26,19 +26,19 @@ function getDefaultModules() {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}'
       },
       {
         test: /\.sass/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}!sass-loader?outputStyle=expanded&indentedSyntax'
       },
       {
         test: /\.scss/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}!sass-loader?outputStyle=expanded'
       },
       {
         test: /\.less/,
-        loader: 'style-loader!css-loader!less-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}!less-loader'
       },
       {
         test: /\.styl/,
@@ -51,6 +51,10 @@ function getDefaultModules() {
       {
         test: /\.(mp4|ogg|svg)$/,
         loader: 'file-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       }
     ]
   };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,36 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.less');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+
+//引入图片的数组数据
+var imageDatas = require('../data/imageDatas.json');
+
+//根据自执行函数图片的json信息获取图片的URL地址
+imageDatas=(function getImageUrl(imageDataArr){
+  for(var i=0,j=imageDataArr.length;i<j;i++){
+    var oneImageData = imageDataArr[i];
+
+    oneImageData.imageData = require('../images' + oneImageData.imgName);
+
+    imageDataArr[i] = oneImageData;
+  }
+  return imageDataArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
+      <section className="stage">
+        <section className="img-sec">
+
+        </section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
+    )
   }
 }
 
